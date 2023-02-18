3b45f727defe47d7343a9054e6582a98e33016b3
kevinorris <stevengerrard.mu@gmail.com>
2023-02-18 18:05:39 +0100

    update

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -62,7 +62,7 @@ const GenericPage: NextPage<props> = ({ id }) => {
     videoRef.current.play();
   };
 
-  return videoInfo["vid-exists"] ? (
+  return videoInfo(
     <div className="h-screen m-auto text-white bg-gray-900">
       <Head>
         <title>Video for {name}</title>
@@ -223,11 +223,6 @@ const GenericPage: NextPage<props> = ({ id }) => {
           </div>
         </div>
       </div>
-    </div>
-  ) : (
-    <div>
-      {" "}
-      <></>
     </div>
   );
 };
